8038644432a7f373a709ea186bfca168ea24d5bf
Shchenkov <155909561+Shchenkov@users.noreply.github.com>
2024-01-16 18:29:58 +0300

    dirty_seqs.R

--- dirty_seqs.R
+++ dirty_seqs.R
@@ -13,7 +13,6 @@ library(phangorn)
 library(catGenes)
 
 # First of all, we should get the sequences to prepare the *.fasta file:
-
 start_csv <- read_csv("start_csv.csv")
 numbers <- as.data.frame(t(start_csv), col.names = c('0'))
 sequences <- read.GenBank(numbers)
@@ -30,13 +29,12 @@ fn <- "sequences_fst.fasta"
    file.remove(fn)
  }
 
- # During the next step, we should prepare the MSA:
-
+# During the next step, we should prepare the MSA:
 Sequences_for_alignment <- readDNAStringSet("final_output.fasta")
 myMuscleAlignment <- msa(Sequences_for_alignment, "Muscle", type = "dna")
 writeXStringSet(as(unmasked(myMuscleAlignment), "XStringSet"), file="./myMuscleAlignment.fasta")
 
-# And now we can trim them:
+# And now we can trim the msa:
 Sequences_for_trimming <- readFasta("myMuscleAlignment.fasta")
 trimmedMuscleAlignment <- msaTrim(Sequences_for_trimming, gap.end = 0.5, gap.mid = 0.9)
 writeFasta(trimmedMuscleAlignment, "trimmedMuscleAlignment.fasta", width = 0)
@@ -46,18 +44,22 @@ fasta_for_nexus <- readFasta('trimmedMuscleAlignment.fasta')
 fasta_for_nexus_df <- as.data.frame(fasta_for_nexus)
 nexusdframe(fasta_for_nexus_df, "trimmedMuscleAlignment.nxs")
 
+# To test the best fitted model:
+mt <- modelTest(fasta)
+best_model <- as.pml(mt)
+best_model
+mt_df <- as.data.frame(mt)
+write.csv(mt_df, file = "model.testing.csv")
+
 # And to write Bayes-block to the end of the nexus-file:
-line="begin mrbayes;
+bayes_block = "begin mrbayes;
 set autoclose=yes nowarn=yes;
 lset covarion=no nucmodel=4by4 nst=6 ngammacat=8 rates=Invgamma; mcmcp ngen=15000000 nruns=2 nchains=4 samplefreq=100 printf=1000 stoprule=n stopval=0.01;
 mcmc;
 END;"
-write(line, file = "trimmedMuscleAlignment.nxs", append = TRUE)
+write(bayes_block, file = "trimmedMuscleAlignment.nxs", append = TRUE)
 
 # Last step - tree building:
 fasta <- read.phyDat("trimmedMuscleAlignment.fasta", format = 'fasta')
-#mt <- modelTest(fasta)
-#best_model <- as.pml(mt)
-#best_model
 tree <- pml_bb(fasta, model="GTR+G(4)+I")
 write.tree(tree$tree, "ML_tree.tre")
